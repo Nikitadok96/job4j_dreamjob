5a15e2589246f4edf0adc80a8088ea525064bcc7
Mingrow <nikitosvozh@gmail.com>
2023-11-15 21:30:03 +0300

    3. Тестирование репозиториев [#504862]

--- src/main/java/ru/job4j/dreamjob/repository/Sql2oCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oCandidateRepository.java
@@ -58,7 +58,8 @@ public class Sql2oCandidateRepository implements CandidateRepository {
                     .addParameter("description", candidate.getDescription())
                     .addParameter("creationDate", candidate.getCreationDate())
                     .addParameter("cityId", candidate.getCityId())
-                    .addParameter("fileId", candidate.getFileId());
+                    .addParameter("fileId", candidate.getFileId())
+                    .addParameter("id", candidate.getId());
             var affectedRows = query.executeUpdate().getResult();
             return affectedRows > 0;
         }

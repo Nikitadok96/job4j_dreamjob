bc81ce6adfe964500c40289c22414df5af4d51fb
Mingrow <nikitosvozh@gmail.com>
2023-11-18 15:06:09 +0300

    4. Многопоточность в базе данных [#504860]

---
+++ src/main/java/ru/job4j/dreamjob/service/SimpleUserService.java
@@ -0,0 +1,27 @@
+package ru.job4j.dreamjob.service;
+
+import org.springframework.stereotype.Service;
+import ru.job4j.dreamjob.model.User;
+import ru.job4j.dreamjob.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class SimpleUserService implements UserService {
+
+    private final UserRepository sql2oUserRepository;
+
+    public SimpleUserService(UserRepository sql2oUserRepository) {
+        this.sql2oUserRepository = sql2oUserRepository;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        return sql2oUserRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        return sql2oUserRepository.findByEmailAndPassword(email, password);
+    }
+}
---
+++ src/test/java/ru/job4j/dreamjob/repository/Sql2oUserRepositoryTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.dreamjob.repository;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.sql2o.Sql2o;
+import ru.job4j.dreamjob.configuration.DatasourceConfiguration;
+import ru.job4j.dreamjob.model.User;
+
+import java.util.Properties;
+
+@Disabled
+class Sql2oUserRepositoryTest {
+    private static Sql2oUserRepository sql2oUserRepository;
+    private static Sql2o sql2o;
+
+    @BeforeAll
+    public static void initRepositories() throws Exception {
+        var properties = new Properties();
+        try (var inputStream = Sql2oUserRepositoryTest.class.getClassLoader().getResourceAsStream("connection.properties")) {
+            properties.load(inputStream);
+        }
+        var url = properties.getProperty("datasource.url");
+        var username = properties.getProperty("datasource.username");
+        var password = properties.getProperty("datasource.password");
+
+        var configuration = new DatasourceConfiguration();
+        var datasource = configuration.connectionPool(url, username, password);
+        sql2o = configuration.databaseClient(datasource);
+
+        sql2oUserRepository = new Sql2oUserRepository(sql2o);
+    }
+
+    @Test
+    public void test() {
+        sql2oUserRepository.save(new User(0, "mail", "name", "pass"));
+    }
+}
